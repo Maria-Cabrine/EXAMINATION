93ed51d1bf3c559f206fde0154f1c4e49b2202f5
Maria-Cabrine <145338532+Maria-Cabrine@users.noreply.github.com>
2023-12-07 13:22:36 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("demo").innerHTML = "NANNYONJO MARIA";
+alert("MARIA");
+function sheila(){
+    document.getElementById(sheila). setAttribute("src","media image. jpg")
+    function 
+}
